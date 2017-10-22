be7e7b8639ad382f9a39d538ad40b4dbb479e03b
smukk9 <smukk9@gmail.com>
2017-10-22 10:30:57 -0700

    define tag service bones

---
+++ services/blog-tag-service/src/main/java/com/sandeep/blogtag/BlogTagServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sandeep.blogtag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTagServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTagServiceApplication.class, args);
+	}
+}
